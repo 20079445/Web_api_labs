970cc36fd067fe2757ab51c706fee438feb64f94
jack murphy <20079445@mail.wit.ie>
2023-04-25 09:34:48 +0100

    Routes for movie and movie details

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,4 +1,3 @@
-import uniqid from 'uniqid'
 import express from 'express';
 import { movies, movieReviews, movieDetails } from './moviesData';
 
@@ -7,22 +6,6 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.json(movies);
 });
-
-// Get movie reviews
-router.get('/:id/reviews', (req, res) => {
-    const id = parseInt(req.params.id);
-    // find reviews in list
-    if (movieReviews.id == id) {
-        res.status(200).json(movieReviews);
-        
-    } else {
-        res.status(404).json({
-            message: 'The resource you requested could not be found.',
-            status_code: 404
-        });
-    }
-});
-
 
 // Get movie details
 router.get('/:id', (req, res) => {
@@ -37,24 +20,6 @@ router.get('/:id', (req, res) => {
     }
 });
 
-//Post a movie review
-router.post('/:id/reviews', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    if (movieReviews.id == id) {
-        req.body.created_at = new Date();
-        req.body.updated_at = new Date();
-        req.body.id = uniqid();
-        movieReviews.results.push(req.body); //push the new review onto the list
-        res.status(201).json(req.body);
-    } else {
-        res.status(404).json({
-            message: 'The resource you requested could not be found.',
-            status_code: 404
-        });
-    }
-});
-
-
 
 export default router;
+
